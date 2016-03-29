3b70cec0bfb5cdc8adb61fadf5a48f34128b1a08
Zach Thomas <zbt5@msstate.edu>
2016-03-29 16:24:37 -0500

    Initial coding of IF and WB stages

--- IF.c
+++ IF.c
@@ -5,4 +5,15 @@
 void IF_stage(uint16_t *PC, uint16_t *instr_mem, IF_ID_Buffer *out_buf)
 {
 	printf("IF\n");
+
+	//Access instruction stored at address held in PC
+	//TODO: Check to see if this offset calculation is correct
+	uint16_t offset_index = *instr_mem - *PC;
+	out_buf->instr = instr_mem[offset_index];
+
+	//Add 4 to PC to get to next instruction
+	*PC += 4;
+
+	//Store new value of PC for next stage
+	out_buf->PC = PC;
 }
--- WB.c
+++ WB.c
@@ -2,7 +2,49 @@
 #include <stdint.h>
 #include <stdio.h>
 
+uint16_t Mux_Mem_to_Reg(uint8_t mem_to_reg)
+{
+	//Choose which data output to write back to register based on control signal
+	uint16_t write_data;
+	if(mem_to_reg == 1)
+	{
+		write_data = mem_data;
+	}
+	else
+	{
+		write_data = alu_data;
+	}
+
+	return write_data;
+}
+
+uint8_t Mux_Reg_Dst(uint8_t reg_dst)
+{
+	//Choose which register to write output to based on control signal
+	uint8_t write_addr
+	if(reg_dst == 0)
+	{
+		write_addr = rt;
+	}
+	else
+	{
+		write_addr = rd;
+	}
+
+	return write_addr;
+}
+
 void WB_stage(MEM_WB_Buffer *in_buf, uint16_t *reg_file)
 {
 	printf("WB\n");
+
+	uint16_t write_data = Mux_Mem_to_Reg(in_buf->mem_to_reg);
+
+	uint8_t write_addr = Mux_Reg_Dst(in_buf->reg_dst);
+
+	//Write data to register at specified address in register file
+	if(in_buf->reg_write == 1)
+	{
+		reg_file[write_addr] = write_data;
+	}
 }
