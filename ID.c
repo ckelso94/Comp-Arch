3b75762e41e7eb7be01b61210ecffc5c662ead0d
samhippie <samhggnbthm@gmail.com>
2016-04-04 17:25:30 -0500

    ID except for control signals

--- ID.c
+++ ID.c
@@ -5,4 +5,8 @@
 void ID_stage(IF_ID_Buffer *in_buf, uint16_t *reg_file, ID_EXE_Buffer *out_buf)
 {
 	printf("ID\n");
+	out_buf->rs = reg_file[(in_buf->instr & 0b000111000000) >> 9];
+	out_buf->rt = reg_file[(in_buf->instr & 0b000000111000) >> 6];
+	out_buf->instr = in_buf->instr;
+	out_buf->PC = in_buf->PC;
 }
