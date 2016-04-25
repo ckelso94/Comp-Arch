dccb0178d8c9ca6689407471a593a6f458a29ff0
samhippie <samhggnbthm@gmail.com>
2016-04-25 17:42:34 -0500

    Revert "screwed up printing"
    This reverts commit c408e3398028471961b4cc9349a961cd428a5607.

---
+++ assembler/out.s
@@ -0,0 +1,9 @@
+# 1 "simple.s"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 1 "<command-line>" 2
+# 1 "simple.s"
+addi $v0, $zero, 2
+addi $v1, $zero, 5
+sub $v2, $v1, $v0
